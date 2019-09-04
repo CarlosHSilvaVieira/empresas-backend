2650aba5187e3e47e8f63ecfaf770a63b74a98b1
Carlos <carlos.h.silva.vieira@gmail.com>
2019-09-04 20:59:46 -0300

    Finish Server Constants

--- src/utils/serverConstants.ts
+++ src/utils/serverConstants.ts
@@ -2,7 +2,14 @@ const ServerConstants = {
 
     version: 'v1',
     key: '45OHRS2KJ763',
-    expires: '30m',
+    expires: '1 hour',
+    port: 3000,
+
+    hostDB: 'localhost',
+    portDB: 3306,
+    userDB: 'root',
+    passDB: 'q1w2e3r4',
+    schemaDB: 'empresas_backend',
 }
 
 export default ServerConstants
